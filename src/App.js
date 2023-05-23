55083b92a11f1d10551ead82637227aca1a2ddd8
GuillermoA231 <andradajoaquin302@gmail.com>
2023-05-23 13:02:40 -0300

    ejercicio 2 terminado

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HelloWorld from './helloWorld';
+
+function App() {
+  return (
+    <div className="App">
+      <HelloWorld saludo="My Friend"/>
+    </div>
+  );
+}
+
+export default App;
